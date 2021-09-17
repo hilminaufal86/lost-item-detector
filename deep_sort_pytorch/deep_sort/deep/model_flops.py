f749773dbcd8e73ceee2b8c1cf09ec24e4c678d5
hilminaufal86 <41366525+hilminaufal86@users.noreply.github.com>
2021-09-17 22:26:15 +0700

    update test file

--- deep_sort_pytorch/deep_sort/deep/model_flops.py
+++ deep_sort_pytorch/deep_sort/deep/model_flops.py
@@ -8,14 +8,18 @@ from model import Net
 import psutil
 
 process = psutil.Process(os.getpid())
-model = Net(reid=True)
 model_path = "checkpoint/ckpt.t7"
+before = process.memory_info().rss / 1024 ** 2
+print('Memory usage before (%.3fMB)' % (process.memory_info().rss / 1024 ** 2))
+model = Net(reid=True)
 state_dict = torch.load(model_path, map_location=torch.device('cpu'))['net_dict']
 model.load_state_dict(state_dict)
-memory_used = 'Memory usage for model load (%.3fMB)' % (process.memory_info().rss / 1024 ** 2)
+after = process.memory_info().rss / 1024 ** 2
+print('Memory usage after (%.3fMB)' % (process.memory_info().rss / 1024 ** 2))
+print('Memory usage to load model (%.3fMB)' % (after - before))
 input = torch.randn(1, 3, 640, 640)
 macs, params = profile(model, inputs=(input,),)
 macs, params = clever_format([macs, params], '%.3f')
 print(macs)
 print(params)
-print(memory_used)
+# print(memory_used)
--- onnx_flops_eval.py
+++ onnx_flops_eval.py
@@ -1,9 +1,25 @@
-from onnx_opcounter import calculate_params, calculate_macs
-import onnx
+# from onnx_opcounter import calculate_params, calculate_macs
+# import onnx
 
-model = onnx.load_model('yolov5/weights/yolov5s_augmented.onnx')
-params = calculate_params(model)
-macs = calculate_macs(model)
+# model = onnx.load_model('yolov5/weights/yolov5s_augmented.onnx')
+# params = calculate_params(model)
+# macs = calculate_macs(model)
 
-print('Number of params:', params)
-print('MACs:', macs)
+# print('Number of params:', params)
+# print('MACs:', macs)
+
+import onnxruntime
+# import sys
+# sys.path.insert(0, './yolov5')
+# from yolov5.models.experimental import attempt_load
+import psutil
+import os
+
+process = psutil.Process(os.getpid())
+# before = process.memory_info().rss / 1024 ** 2
+# print('Memory usage before (%.3fMB)' % (before))
+session = onnxruntime.InferenceSession('yolov5/weights/yolov5s_augmented.onnx', None)
+# model = attempt_load('yolov5/weights/yolov5l_augmented.pt', map_location='cpu')
+after = process.memory_info().rss / 1024 ** 2
+print('Memory usage after (%.3fMB)' % (after))
+# print('Memory usage to load model (%.3fMB)' % (after - before))
